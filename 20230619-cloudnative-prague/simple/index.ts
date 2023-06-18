8373de9691b721943eff8448fc98006307054bd0
Ringo De Smet <ringo@de-smet.name>
2023-06-18 19:01:35 +0200

    Typescript policy pack

--- 20230619-cloudnative-prague/simple/index.ts
+++ 20230619-cloudnative-prague/simple/index.ts
@@ -1,9 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import { CannedAcl } from "@pulumi/aws/s3";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket",
+{
+    acl: CannedAcl.PublicRead
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
